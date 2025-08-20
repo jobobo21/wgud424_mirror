f0df0b1c89ba50029fcc6b6fa595d2911a480cb3
Johan Teller <j_teller@zoho.com>
2025-08-20 16:56:11 -0400

    connecting to database

--- express/server.js
+++ express/server.js
@@ -2,11 +2,20 @@ const express = require("express");
 const app = express();
 
 const port = process.env.PORT || 5000;
-
+const sequelize = new Sequelize('mysql://'+process.env.DATABASE_URL) // Example for MYSQL
+var status = "Not Connected"
+try {
+  await sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+  status = "Connected To Database"
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
 app.get("/", (req, res) => {
   return res.status(200).send({
     message: "Hello World!",
-    title: process.env.Title
+    title: process.env.Title,
+    status
   });
 });
 
