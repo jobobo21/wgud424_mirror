677732d0734a1525e50c2bd10a1994af0a0ea8e1
Johan Teller <j_teller@zoho.com>
2025-09-07 23:11:40 -0400

    added express.json middleware for parsing json requests

--- express/server.js
+++ express/server.js
@@ -9,7 +9,7 @@ import student_course from "./routes/student_course.js";
 import student_assessment from "./routes/student_assessments.js";
 dotenv.config();
 const app = express();
-
+app.use(express.json());
 const port = process.env.PORT || 5000;
 // Example for MYSQL
 
@@ -21,6 +21,7 @@ app.get("/", (req, res) => {
   });
 });
 app.use("/login", login);
+
 app.use("/course", course);
 app.use("/user", user);
 app.use("/terms", term);
